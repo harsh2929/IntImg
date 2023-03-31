afe247330721a54def3da93e22a393bc9a4d6c9e
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-31 08:45:21 +0530

    Updated install.py
    Did more indentation
    Added error handling.
    updated depricated modules

--- src/Python/install.py
+++ src/Python/install.py
@@ -1,43 +1,39 @@
 #!/usr/bin/env python
 
-import setuptools
-from distutils.core import setup
 import os
-import numpy
 import platform
-import sys
-intimg_version=os.environ['VERSION']
+import numpy
+from setuptools import setup, Extension
 
-dir= [numpy.get_include(), library_include_path]
-compile = []
-extra = [library_lib_path]
-arguments = []
-link = []
-extra_libraries = ['intimg']
+intimg_version = os.environ.get('VERSION')
+if not intimg_version:
+    raise ValueError("VERSION environment variable is not set.")
 
-print ("extra_library_dirs " , extra_library_dirs)
-
-extra_include_dirs += [os.path.join(".." , "Core"),
-                       os.path.join(".." , "Core",  "functions_CPU"),
-						   "."]
-
+numpy_include_path = numpy.get_include()
+library_include_path = "/path/to/library/include"
+library_lib_path = "/path/to/library/lib"
+extra_include_dirs = [os.path.join("..", "Core"),
+                      os.path.join("..", "Core", "functions_CPU"),
+                      "."]
 if platform.system() == 'Windows':
-    compile[0:] = ['/DWIN32','/EHsc','/DBOOST_ALL_NO_LIB' , '/openmp' ]
+    compile_args = ['/DWIN32', '/EHsc', '/DBOOST_ALL_NO_LIB', '/openmp']
 else:
-    extra_compile_args = ['-fopenmp','-O2', '-funsigned-char', '-Wall', '-std=c++0x']
-    extra_libraries += [@EXTRA_OMP_LIB@]
+    compile_args = ['-fopenmp', '-O2', '-funsigned-char', '-Wall', '-std=c++0x']
+    extra_libraries = ['omp']
+
+extra_include_dirs += [numpy_include_path, library_include_path]
+extra_library_dirs = [library_lib_path]
+
+extension = Extension(name="",
+                      sources=[os.path.join(".", "src", "cpu_wrappers.pyx")],
+                      include_dirs=extra_include_dirs,
+                      library_dirs=extra_library_dirs,
+                      extra_compile_args=compile_args,
+                      libraries=['intimg'])
 
 setup(
     version=intimg_version,
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("",
-                             sources=[os.path.join("." , "src", "cpu_wrappers.pyx") ],
-                             include_dirs=extra,
-			     library_dirs=dir,
-        		     extra_compile_args=extra_compile_args,
-			     libraries=extra_libraries ),
-
-    ],
-	zip_safe = False,
-	packages=['intimg', 'intimg.supp'],
+    ext_modules=[extension],
+    zip_safe=False,
+    packages=['intimg', 'intimg.supp']
 )
